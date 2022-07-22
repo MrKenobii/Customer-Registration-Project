fed1cd265916c8ebe8314ae11efc4d524876faf3
MrKenobii <anldygc35@windowslive.com>
2022-07-22 18:36:09 +0300

    adding css

--- src/main/java/com/anilduyguc/customerregistration/controller/CustomerController.java
+++ src/main/java/com/anilduyguc/customerregistration/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.anilduyguc.customerregistration.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,7 +16,7 @@ public class CustomerController {
     @Autowired
     CustomerDAO customerDAO;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String getCustomerList(Model model) {
         List<Customer> customers = customerDAO.getCustomers();
         model.addAttribute("customers", customers);
